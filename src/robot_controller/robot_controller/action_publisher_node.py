566323b4f30d1365723065de7bd2701f16d4f040
Victor Dhedin <victor.dhedin@tum.de>
2025-02-17 10:34:44 +0000

    Move files. Add interface.

---
+++ src/robot_controller/robot_controller/action_publisher_node.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from robot_controller_interface.msg import RobotActions
+
+class ActionPublisherNode(Node):
+    def __init__(self):
+        super().__init__('action_publisher_node')
+        self.publisher = self.create_publisher(RobotActions, '/robot_actions', 10)
+        self.timer = self.create_timer(0.1, self.publish_actions)  # 10 Hz
+
+    def publish_actions(self):
+        msg = RobotActions()
+        msg.target_joint_positions = [1.0, 1.0, 1.0]  # Example data
+        msg.target_joint_velocities = [0.0, 0.0, 0.0]  # Example data
+        msg.target_joint_torques = [0.0, 0.0, 0.0]  # Example data
+        self.publisher.publish(msg)
+        self.get_logger().info('Publishing robot actions')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ActionPublisherNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
